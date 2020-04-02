be8ed8e1ee63f422587fac8439ddf787ebabe61c
lingxt <lxt9601@gmail.com>
2020-04-03 03:05:43 +0900

    fix index.html

--- crawler/templatetags/crawler_extras.py
+++ crawler/templatetags/crawler_extras.py
@@ -9,7 +9,7 @@ def price2string(price):
         return '{:20,d}'.format(int(price))
     else:
         return None
-# @register.simple_tag
-# def get_total_num(wanted_item):
-#     catched_items = wanted_item.catcheditem_set.all()
-#     return sum([item.store_item.num for item in catched_items])
+@register.simple_tag
+def get_total_num(wanted_item):
+    store_items = wanted_item.store_items.all()
+    return sum([item.num for item in store_items])
